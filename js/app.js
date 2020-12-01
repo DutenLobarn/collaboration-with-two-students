e318b7893420d472f46b5224847eddded034e89c
Nina-FEU20 <70330982+Nina-FEU20@users.noreply.github.com>
2020-12-01 12:15:59 +0100

    Added template for HTML&CSS
    Uppdatering i app.js är för att ni ska kunna se hur det ser ut med korten.

--- js/app.js
+++ js/app.js
@@ -4,3 +4,22 @@ console.log('Nina skapar detta lokalt på datorn');
 console.log('Nina Testar att göra en commit från en annan branch'); 
 
 console.log('Det här skrivs i samuelsbranch :)');
+
+/* bara för att ni ska se hur korten ser ut när de skapas på domen! */
+
+let gameboard = document.querySelector('.gameboard')
+for (let i = 0; i < 24; i++) {
+    let card = document.createElement('div')
+    card.classList.add('card')
+
+    let frontCard = document.createElement('div');
+    frontCard.classList.add('front')
+
+    let backCard = document.createElement('div');
+    backCard.classList.add('back')
+    backCard.innerText = "Här ska bilderna från API-vara"
+
+    gameboard.appendChild(card);
+    card.appendChild(frontCard)
+    card.appendChild(backCard);
+}
