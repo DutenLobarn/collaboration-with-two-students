03791cc6206bbe3d64e954a900e961579ea7a0bc
Samuel Meijer <Samuel.j.meijer@gmail.com>
2020-12-01 16:25:14 +0100

    Selecting elements and adding variables for basic functionality

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -1,1 +1,27 @@
 // MODULE MemoryGAME
+'use strict'
+
+// TODO: Add imports from API and MemoryCard!
+
+
+
+// ***** ELEMENTS *****
+const gameboard = document.querySelector('.gameboard');
+const playerOneTotalScore = document.querySelector('.pone-total-score');
+const playerTwoTotalScore = document.querySelector('.ptwo-total-score');
+const playerOneCurrentScore = document.querySelector('.score-player-one');
+const playerTwoCurrentScore = document.querySelector('.score-player-two');
+
+// ***** GLOBAL VARIABLES *****
+let totalGames = 0;
+// TODO: Who starts?
+let currentPlayer = null;
+let firstPickedCard = null;
+let secondPickedCard = null;
+
+// TODO: Move to app.js!
+const pressToPlay = document.querySelector('.play');
+pressToPlay.addEventListener('click', function (event) {
+   // Todo: Start the game.
+   console.log('Start the game!'); 
+});
