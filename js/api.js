30255f4d197636c0d9accc3ac6f5f348b7a5a2c6
Nina-FEU20 <70330982+Nina-FEU20@users.noreply.github.com>
2020-12-03 14:15:04 +0100

    Updated API-module
    Tog bort temporär konstruktor.
    tog bort skapandet av memory-kortet(flyttat till riktiga konstruktorn). Tog bort shuffling-metod. La till parameter för fetchImages

--- js/api.js
+++ js/api.js
@@ -1,24 +1,10 @@
-/* Getting Element from DOM */
-let gameboard = document.querySelector('.gameboard')
+/* Getting Element from DOM - no need for this anymore */ 
+// let gameboard = document.querySelector('.gameboard')
 
-/* Temporary Constructor, should be imported from construtor-module */ 
-function Card(_imgSrc) {
-    this.imgSrc = _imgSrc
-}
+/* Removed temporary constructor */
 
-Card.prototype.flip = function(){
-    this.classList.add('flip')
-}
-Card.prototype.flipback = function(){
-    this.classList.remove('flip')
-}
-
-
-fetchImages(); 
-
-/* Main function to fetch images */
-// Can I just export this function??? 
-function fetchImages() {
+/* Main function to fetch images, the parameter is to send in the array of the memoryobjects*/
+function fetchImages(cardArr) {
 
     const key = 'e97530c4db33a4ae21d65f765fe9c551'
     let theme = 'dog'
@@ -35,30 +21,32 @@ function fetchImages() {
         let arrayOfUrls = []; 
         for (let i = 0; i < 12; i++) {
             arrayOfUrls.push(getImageUrl(data.photos.photo[i]));
+            arrayOfUrls.push(getImageUrl(data.photos.photo[i])) // added one more so we get two of each url.
         }
 
-        
-        /* Creating an array with the card-constructor with the img-url as argument, two of every url */
-        let arrayOfCards = []; 
-        for(let i = 0; i < 12; i++) {
-            arrayOfCards.push(new Card(arrayOfUrls[i]))
-            arrayOfCards.push(new Card(arrayOfUrls[i]))
+        /* Using the parameter to send the url's into the memoryobject */
+        for(let i = 0; i < cardArr.length; i++) {
+            cardArr[i].imgSrc = arrayOfUrls[i];
         }
 
-        /* Shuffling the Cards */
-        let shuffledArrayOfCards = arrayOfCards.sort(() => Math.random() -0.5); 
+        /* Moving creation of cards to memory module */
+        // let arrayOfCards = []; 
+        // for(let i = 0; i < 12; i++) {
+        //     arrayOfCards.push(new Card(arrayOfUrls[i]))
+        //     arrayOfCards.push(new Card(arrayOfUrls[i]))
+        // }
+
+        /* Shuffling of the cards added to the constructor instead! */
+        // let shuffledArrayOfCards = arrayOfCards.sort(() => Math.random() -0.5); 
     
-        /* Putting the cards on the DOM */ 
-        createCards(shuffledArrayOfCards); 
-
+        // /* Putting the cards on the DOM ---- MOVED */ 
+        // createCards(shuffledArrayOfCards); 
     })
     .catch((err) => {
         console.log(err)
 
     })
-
 }
-
 
 function getImageUrl(photoObject) {
 
@@ -70,24 +58,26 @@ function getImageUrl(photoObject) {
     return imgUrl; 
 }
 
-function createCards(arr){
-    for (let i = 0; i < 24; i++) {
-        let card = document.createElement('div')
-        card.classList.add('card')
+// Skapandet av kort flyttas till prototyp
+// function createCards(arr){
+//     for (let i = 0; i < 24; i++) {
+//         let card = document.createElement('div')
+//         card.classList.add('card')
     
-        let frontCard = document.createElement('div');
-        frontCard.classList.add('front')
+//         let frontCard = document.createElement('div');
+//         frontCard.classList.add('front')
     
-        let dogImg = document.createElement('img');
-        dogImg.classList.add('back')
+//         let dogImg = document.createElement('img');
+//         dogImg.classList.add('back')
         
-        /* Here I add the img-url as the src to the backside of the memorycard */
-        dogImg.src = arr[i].imgSrc; 
+//         /* Here I add the img-url as the src to the backside of the memorycard */
+//         dogImg.src = arr[i].imgSrc; 
     
-        gameboard.appendChild(card);
-        card.appendChild(frontCard)
-        card.appendChild(dogImg);
-    }
-}
+//         gameboard.appendChild(card);
+//         card.appendChild(frontCard)
+//         card.appendChild(dogImg);
+//     }
+// }
+
 
 
