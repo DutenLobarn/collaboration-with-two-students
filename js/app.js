1c04547eaa324c1f3d8bcc44a07fc651ecc36174
Nina-FEU20 <70330982+Nina-FEU20@users.noreply.github.com>
2020-12-04 16:00:47 +0100

    Changed eventlistener to Press Play for names
    Small update to CSS
    added maxwidth

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
-let input = document.querySelectorAll('input')
 let playerOne = document.querySelector('.player-one')
 let playerTwo = document.querySelector('.player-two')
+
 playerOne.style.display = 'none'; 
 playerTwo.style.display = 'none'; 
+let pressToPlay = document.querySelector('.play h3'); 
 
-input[0].addEventListener('keypress', function (e) {
-    if (e.key == 'Enter' && e.target.value != '') {
-        playerOne.style.display = 'block'; 
-        playerOne.innerText = e.target.value; 
-        input[0].style.display = 'none'
-    }
+pressToPlay.addEventListener('click', function(e) {
+    enterNames(); 
 })
 
-input[1].addEventListener('keypress', function (e) {
-    if (e.key == 'Enter' && e.target.value != '') {
-        playerTwo.style.display = 'block'; 
-        playerTwo.innerText = e.target.value;
-        input[1].style.display = 'none'
+
+
+function enterNames(){
+    let input = document.querySelectorAll('input')
+    
+    if (input[0].value.length !== 0) {
+        playerOne.innerText = input[0].value;
+    } else {
+        playerOne.innerText = 'Player One'
     }
-})
+
+    if (input[1].value.length !== 0) {
+        playerTwo.innerText = input[1].value;
+    } else {
+        playerTwo.innerText = 'Player Two'
+    }
+    
+    playerOne.style.display = 'block'; 
+    playerTwo.style.display = 'block'; 
+
+    input[0].style.display = 'none'
+    input[1].style.display = 'none'
+}
+
