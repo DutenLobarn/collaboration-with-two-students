00e71007ef3b921a9c30a52f05a6925f546883ac
Nina-FEU20 <70330982+Nina-FEU20@users.noreply.github.com>
2020-12-04 13:06:34 +0100

    added errorhandling
    Removed unnessacery comments

--- js/api.js
+++ js/api.js
@@ -1,53 +1,61 @@
-/* Getting Element from DOM - no need for this anymore */ 
-// let gameboard = document.querySelector('.gameboard')
-
-/* Removed temporary constructor */
-
 /* Main function to fetch images, the parameter is to send in the array of the memoryobjects*/
 export function fetchImages(cardArr) {
 
     const key = 'e97530c4db33a4ae21d65f765fe9c551'
-    let theme = 'dog'
+    let theme = 'animal'
     
     const url = `https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${theme}&format=json&nojsoncallback=1&page=2&page=1`; 
 
     fetch(url)
-    .then((response) => response.json())
+    .then((response) =>{
+        return response.json(); 
+    }) 
     .then((data) => {
-
-        // TODO: ADD ERRORHANDLING 
         
-        /* Creating array and then push 12 different img-urls into it */
-        let arrayOfUrls = []; 
-        for (let i = 0; i < 12; i++) {
-            arrayOfUrls.push(getImageUrl(data.photos.photo[i]));
-            arrayOfUrls.push(getImageUrl(data.photos.photo[i])) // added one more so we get two of each url.
+        // If no photos are found, for example if "theme" is not a valid seachtext
+        if (data.stat === 'ok' && data.photos.photo.length === 0) {
+            throw 'No photos found'; 
+        }
+        
+        //If everything is okey! 
+        else if (data.stat === 'ok') {
+            console.log(data)
+            /* Creating array and then push 12 different img-urls into it */
+            let arrayOfUrls = []; 
+            for (let i = 0; i < 84; i += 7) {
+                arrayOfUrls.push(getImageUrl(data.photos.photo[i]));
+                arrayOfUrls.push(getImageUrl(data.photos.photo[i])) // added one more so we get two of each url.
+            } 
+
+            console.log(arrayOfUrls); 
+
+            /* Using the parameter to send the url's into the memoryobject */
+            for(let i = 0; i < cardArr.length; i++) {
+                cardArr[i].imgSrc = arrayOfUrls[i];
+            }
+        
+        // if API-key is invalid    
+        } else if (data.stat === 'fail') {
+            throw data.message; 
         }
 
-        /* Using the parameter to send the url's into the memoryobject */
-        for(let i = 0; i < cardArr.length; i++) {
-            cardArr[i].imgSrc = arrayOfUrls[i];
-        }
-
-        /* Moving creation of cards to memory module */
-        // let arrayOfCards = []; 
-        // for(let i = 0; i < 12; i++) {
-        //     arrayOfCards.push(new Card(arrayOfUrls[i]))
-        //     arrayOfCards.push(new Card(arrayOfUrls[i]))
-        // }
-
-        /* Shuffling of the cards added to the constructor instead! */
-        // let shuffledArrayOfCards = arrayOfCards.sort(() => Math.random() -0.5); 
-    
-        // /* Putting the cards on the DOM ---- MOVED */ 
-        // createCards(shuffledArrayOfCards); 
     })
     .catch((err) => {
-        console.log(err)
+        // display error on DOM
+        let errorMessage = document.createElement('p')
+        errorMessage.style.fontSize = '1.3rem'; 
+        errorMessage.style.marginTop = '1rem'
+        errorMessage.style.textAlign = 'center'
+        let startPlay = document.querySelector('.play')
+        startPlay.appendChild(errorMessage); 
 
+        // showing error message + try again!
+        errorMessage.innerText = `${err}, try again!`; 
+        console.log(err); 
     })
 }
 
+// function to get the imgUrl that we send in to the parameter for the memoryobjects
 function getImageUrl(photoObject) {
 
     let photo = photoObject;
@@ -57,24 +65,3 @@ function getImageUrl(photoObject) {
 
     return imgUrl; 
 }
-
-// Skapandet av kort flyttas till prototyp
-// function createCards(arr){
-//     for (let i = 0; i < 24; i++) {
-//         let card = document.createElement('div')
-//         card.classList.add('card')
-    
-//         let frontCard = document.createElement('div');
-//         frontCard.classList.add('front')
-    
-//         let dogImg = document.createElement('img');
-//         dogImg.classList.add('back')
-        
-//         /* Here I add the img-url as the src to the backside of the memorycard */
-//         dogImg.src = arr[i].imgSrc; 
-    
-//         gameboard.appendChild(card);
-//         card.appendChild(frontCard)
-//         card.appendChild(dogImg);
-//     }
-// }
