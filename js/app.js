80d4cb71f7da9fd4d415f750848ea747792ee551
Nina-FEU20 <70330982+Nina-FEU20@users.noreply.github.com>
2020-12-04 14:26:30 +0100

    added inputs for name and updated css
    added animation for "press play"

--- js/app.js
+++ js/app.js
@@ -1,25 +1,21 @@
-console.log('Hello World!');
-console.log('Nina skapar detta lokalt på datorn'); 
+let input = document.querySelectorAll('input')
+let playerOne = document.querySelector('.player-one')
+let playerTwo = document.querySelector('.player-two')
+playerOne.style.display = 'none'; 
+playerTwo.style.display = 'none'; 
 
-console.log('Nina Testar att göra en commit från en annan branch'); 
+input[0].addEventListener('keypress', function (e) {
+    if (e.key == 'Enter' && e.target.value != '') {
+        playerOne.style.display = 'block'; 
+        playerOne.innerText = e.target.value; 
+        input[0].style.display = 'none'
+    }
+})
 
-console.log('Det här skrivs i samuelsbranch :)');
-
-/* bara för att ni ska se hur korten ser ut när de skapas på domen! */
-
-let gameboard = document.querySelector('.gameboard')
-for (let i = 0; i < 24; i++) {
-    let card = document.createElement('div')
-    card.classList.add('card')
-
-    let frontCard = document.createElement('div');
-    frontCard.classList.add('front')
-
-    let backCard = document.createElement('div');
-    backCard.classList.add('back')
-    backCard.innerText = "Här ska bilderna från API-vara"
-
-    gameboard.appendChild(card);
-    card.appendChild(frontCard)
-    card.appendChild(backCard);
-}
+input[1].addEventListener('keypress', function (e) {
+    if (e.key == 'Enter' && e.target.value != '') {
+        playerTwo.style.display = 'block'; 
+        playerTwo.innerText = e.target.value;
+        input[1].style.display = 'none'
+    }
+})
